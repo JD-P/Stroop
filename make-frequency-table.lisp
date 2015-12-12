7c13fa16f856158629525e8f70fbcea50ef9ed57
John David Pressman <jdpostage@linuxmail.org>
2015-12-11 23:20:40 -0800

    Moved all of these files.

--- make-frequency-table.lisp
+++
@@ -1,4 +0,0 @@
-(load "table-gen.lisp")
-(setq output (gen-frequency-table *standard-input*))
-(loop for key being the hash-keys in output using (hash-value value) 
-   do (format t "(~A ~A)~%" key value))
