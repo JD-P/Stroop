29743cf1dfe3d563d1576a904b5ccf4f0d906b11
John David Pressman <jdpostage@linuxmail.org>
2015-12-11 20:11:24 -0800

    Changed maphash call to loop.

--- make-frequency-table.lisp
+++ make-frequency-table.lisp
@@ -1,4 +1,4 @@
 (load "table-gen.lisp")
 (setq output (gen-frequency-table *standard-input*))
-(maphash #'(lambda (key value) (format t "(~A ~A)~%" key value))
-	 output)
+(loop for key being the hash-keys in output using (hash-value value) 
+   do (format t "(~A ~A)~%" key value))
