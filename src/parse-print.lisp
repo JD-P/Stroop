2950fc3410eb768ecd29c95c78397ccfd8979af2
John David Pressman <jdpostage@linuxmail.org>
2015-12-17 02:50:56 -0800

    Finished stroop printing infrastructure for colorizing words.

--- src/parse-print.lisp
+++ src/parse-print.lisp
@@ -2,14 +2,16 @@
 (defpackage stroop.parse-print
   (:use :cl
 	:closure-html)
-  (:export :pt-print))
+  (:export :pt-print
+	   :stroop-printer
+	   :stroop-excluder))
 (in-package :stroop.parse-print)
 
 ;; Take a parse tree and descend it in-order, at each stage of the descent print
 ;; element beginning tag before descending to next element(s) and element 
 ;; ending tag after.
 
-(defun pt-print (parse-tree, printer &optional excluder)
+(defun pt-print (parse-tree printer &optional excluder)
   "pt-print takes three arguments:
 
    parse-tree: An HTML element returned by closure-html:parse or 
@@ -23,11 +25,36 @@
              printing.
   "
   (if (not excluder) (setq excluder (lambda () NIL)) NIL)
-  (if (excluder parse-tree) NIL
-      (format t "<~a>" (pt-name element)) ;; Print beginning tag of element
-      (mapc #'(lambda (element)
-		(if (printer element) 
-		    NIL 
-		    (pt-print element printer)))
-	    (pt-children parse-tree))
-      (format t "</~a>" (pt-name element)))) ;; Print ending tag of element 
+  (if (funcall excluder parse-tree) NIL
+      (progn
+	(format t "<~a~a>" (pt-name parse-tree) ;; Print beginning tag of element
+		(apply #'concatenate 'string 
+		       (reverse (stringify-attrs (pt-attrs parse-tree)))))
+	(mapc #'(lambda (element)
+		  (if (funcall printer element) 
+		      NIL 
+		      (pt-print element printer excluder)))
+	      (pt-children parse-tree))
+	(format t "</~a>" (pt-name parse-tree))))) ;; Print ending tag of element 
+
+(defun stringify-attrs (attr-list &optional strings)
+  (if (not (first attr-list)) strings
+      (stringify-attrs
+       (subseq attr-list 2)
+       (cons 
+	(format nil " ~a='~a'"
+		(first attr-list) 
+		(second attr-list))
+	strings))))
+
+(defun stroop-printer (element)
+  (if (equal (pt-name element) :PCDATA)
+      (progn 
+	(print (pt-attrs element))
+	T)
+      NIL))
+  
+(defun stroop-excluder (element)
+  (if (equal (pt-name element) :LINK)
+      T
+      NIL))
